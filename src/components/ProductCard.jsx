bc64cfd13bfa2da66301c8616eabb1b86735b242
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-05 18:35:29 +0530

    add qty function

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { ShoppingBag } from "lucide-react";
+import { useState } from "react";
 
-const ProductCard = ({ product, AddBtn, setQuntity }) => {
+const ProductCard = ({ product, AddBtn, setProductsQnt }) => {
+  const [quntity, setQuntity] = useState(0);
+
+  function handleQtyClick(arg) {
+    setQuntity((prev) => {
+      if (arg === "plus" && prev < 10) return prev + 1;
+      else if (arg === "min" && prev > 1) return prev - 1;
+      return prev;
+    });
+  }
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+
+    if (value === "") {
+      setQuntity("");
+      return;
+    }
+    const num = Number(value);
+    if (!isNaN(num) && num >= 1 && num <= 10) {
+      setQuntity(num);
+    }
+  }
+
   return (
     <div className="border-1 flex flex-col items-center justify-center py-3 px-5 rounded-lg">
       <img src={product.image} className="h-50" alt="" />
@@ -12,22 +36,24 @@ const ProductCard = ({ product, AddBtn, setQuntity }) => {
           <p className="text-xl text-start mr-5">${product.price}</p>
           <div className="border border-black px-2 py-1 rounded-2xl flex items-center">
             <span
-              onClick={() => setQuntity((prev) => prev + 1)}
+              onClick={() => handleQtyClick("min")}
               className="text-xl cursor-pointer active:scale-95 transition-transform"
             >
-              +
+              -
             </span>
             <input
               type="number"
               min={1}
+              value={quntity}
+              onChange={handleInputChange}
               max={10}
               className="bg-white text-black w-8 border-none focus:border-none outline-none ml-4"
             />
             <span
-              onClick={() => setQuntity((prev) => prev - 1)}
+              onClick={() => handleQtyClick("plus")}
               className="text-xl cursor-pointer active:scale-95 transition-transform"
             >
-              -
+              +
             </span>
           </div>
         </div>
@@ -35,7 +61,7 @@ const ProductCard = ({ product, AddBtn, setQuntity }) => {
           onClick={() => AddBtn(product.id)}
           className="flex bg-blue-800 text-white py-2 text-xl items-center px-5 rounded-full gap-3 active:scale-95 transition-transform duration-300"
         >
-          Add To Cart <ShoppingBag />
+          Add To Cart
         </button>
       </div>
     </div>
