3fc6fd8d9b8bb3ddd74624a8a9a7261563b63e54
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-05 21:51:53 +0530

    add addToCart function to layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,22 +2,37 @@ import { Header } from "./components/Header";
 import { Outlet, useLoaderData } from "react-router-dom";
 import Footer from "./components/Footer";
 import { useEffect, useState } from "react";
-
 const Layout = () => {
   const { productData } = useLoaderData();
   const [cart, setCart] = useState([]);
-  const [cartProductQtys, setCartProductQtys] = useState({});
+
+  const onAddToCart = (product, qty) => {
+    setCart((prev) => {
+      const existing = prev.find((p) => p.id === product.id);
+      if (existing) {
+        prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + qty } : item
+        );
+      }
+      return [...prev, { ...prev, qty }];
+    });
+  };
 
   useEffect(() => {
-    console.log("Cart Product Qty Updated", cartProductQtys);
-  }, [cartProductQtys]);
+    console.log("Cart Product Qty Updated", cart);
+  }, [cart]);
 
   return (
     <div className="flex flex-col min-h-screen font-poppins">
       <Header cart={cart} />
       <main className="flex-1 p-5">
         <Outlet
-          context={{ productData, setCartProductQtys }}
+          context={{
+            productData,
+            setCart,
+            cart,
+            onAddToCart,
+          }}
         />
       </main>
       <Footer />
