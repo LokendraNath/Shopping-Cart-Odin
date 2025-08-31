01a304b40946ac8245bcf841f1ef56b07ad11596
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-31 11:04:59 +0530

    refacter Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,27 @@
 import { CircleUserRound, ShoppingCart } from "lucide-react";
 import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { ShopContext } from "../Context/ShopContext";
+import { ShopContext } from "../Context/ShopContext.js";
 
-const Links = () => {
-  const { cart } = useContext(ShopContext);
+const Links = ({ cartItemCount }: { cartItemCount: number }) => {
   return (
     <ul className="relative">
       <Link to="cart">
         <ShoppingCart size={30} className="cursor-pointer" />
       </Link>
-      {cart.length ? (
+      {cartItemCount ? (
         <div className="bg-blue-500 absolute top-[-6px] right-[-2px] w-5 h-5 flex items-center justify-center rounded-full text-white">
-          {cart.length}
+          {cartItemCount}
         </div>
       ) : (
         ""
       )}
+      ""
     </ul>
   );
 };
 
-export const Header = ({ cartItemCount }) => {
+export const Header = ({ cartItemCount }: { cartItemCount: number }) => {
   return (
     <header className="py-5 flex items-center justify-between px-20 text-blue-800">
       <h1 className="text-3xl font-bold">
