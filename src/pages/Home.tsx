3e96fabaa3cb77107deeaaf031115e1558d9a994
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-01 12:20:54 +0530

    udjust home page text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,20 +13,17 @@ const Home = () => {
         />
       </div>
       <div className="md:w-1/2 flex flex-col items-center justify-center text-center md:text-start">
-        <p className="text-xl md:text-xl mb-2 font-bold md:mb-4 tracking-widest">
+        <p className="text-xl md:text-xl mb-2 font-bold md:mb-2 tracking-widest">
           Welcome To{" "}
         </p>
-        <h1 className="text-6xl md:text-7xl mt-2 md:mt-3 font-bold block tracking-normal">
-          Quick
-          <span className="whitespace-nowrap overflow-hidden basket">
-            Basket
-          </span>
+        <h1 className="text-6xl md:text-7xl font-bold block tracking-normal">
+          QuickBasket
         </h1>
-        <p className="text-sm md:text-lg tracking-wide mb-5 md:mb-10 font-semibold text-blue-500 mt-3 w-80">
+        <p className="text-sm md:text-lg tracking-wide mb-5 md:mb-7 font-semibold text-blue-500 mt-3 w-80 md:w-96 md:text-center">
           Smart, seamless shopping starts here. Quick, easy, and always
           affordable.
         </p>
-        <button className="mt-5 md:mt-7 py-2 md:py-4 px-5 md:px-10 bg-gradient-to-l to-blue-500 from-blue-800 rounded-xl md:rounded-2xl text-xl md:text-2xl font-bold text-white group transition-transform duration-300">
+        <button className="mt-2 md:mt-3 py-2 md:py-4 px-5 md:px-10 bg-gradient-to-l to-blue-500 from-blue-800 rounded-xl md:rounded-2xl text-xl md:text-2xl font-bold text-white group transition-transform duration-300">
           <Link to="products" className="flex items-center gap-4">
             Shop Now{" "}
             <ArrowRightIcon className="transform transition-transform duration-300 group-hover:translate-x-3" />
