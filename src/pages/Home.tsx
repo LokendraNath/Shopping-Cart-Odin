006a1ab15a84c35582a07fdcc5d85be27bf91276
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-01 09:24:58 +0530

    home responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,28 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex h-full w-full text-blue-900">
-      <div className="w-1/2 flex items-center justify-center">
+    <div className="h-[calc(100vh-64px)] w-full text-blue-900 flex flex-col md:flex-row items-center justify-center">
+      <div className="hidden md:w-1/2 md:flex items-center justify-center">
         <img
           src="https://img.freepik.com/premium-vector/happy-couple-shopping-with-discounts-sales_123891-155782.jpg"
           className="w-[80%]"
         />
       </div>
-      <div className="w-1/2 flex flex-col items-start justify-center pl-20">
-        <h1 className="text-2xl font-bold mb-4">
-          Welcome To <span className="text-6xl mt-3 block">QuickBasket</span>
+      <div className="md:w-1/2 flex flex-col items-center justify-center text-center md:text-start">
+        <p className="text-xl md:text-xl mb-2 font-bold md:mb-4 tracking-widest">
+          Welcome To{" "}
+        </p>
+        <h1 className="text-6xl md:text-7xl mt-2 md:mt-3 font-bold block tracking-normal">
+          Quick
+          <span className="whitespace-nowrap overflow-hidden basket">
+            Basket
+          </span>
         </h1>
-        <p className="text-lg tracking-wide mb-10 font-semibold text-blue-500 mt-3">
+        <p className="text-sm md:text-lg tracking-wide mb-5 md:mb-10 font-semibold text-blue-500 mt-3 w-80">
           Smart, seamless shopping starts here. Quick, easy, and always
           affordable.
         </p>
-        <button className="py-4 px-10 bg-gradient-to-l to-blue-500 from-blue-800 rounded-2xl text-2xl font-bold text-white group transition-transform duration-300">
+        <button className="mt-5 md:mt-7 py-2 md:py-4 px-5 md:px-10 bg-gradient-to-l to-blue-500 from-blue-800 rounded-xl md:rounded-2xl text-xl md:text-2xl font-bold text-white group transition-transform duration-300">
           <Link to="products" className="flex items-center gap-4">
             Shop Now{" "}
             <ArrowRightIcon className="transform transition-transform duration-300 group-hover:translate-x-3" />
